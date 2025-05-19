329f9b2fe98e6ef8b1966dbdc8ac323816cadb55
Harrison <121962835+kobbyharris@users.noreply.github.com>
2025-05-19 06:48:19 -0500

    Add files via upload

---
+++ harrison.work.js
@@ -0,0 +1,39 @@
+
+let number = [1,7, 100, 3, 5, 6, 2000];
+let evenNumbers = [10, 12, 14, 16, 18, 20, 22];
+
+// write a function to find the largest number in an array  
+/* function findLargest(numbers){
+    let largeValue = numbers[0];
+    for(let number of numbers) {
+        if(number > largeValue)
+            largeValue = number;
+    }    
+
+    return largeValue;
+}
+
+
+console.log(findLargest(number));
+console.log(findLargest(evenNumbers)); */
+
+
+//write a function to reverse an array without using reverse
+/* function reverseArray(numbers) {
+    let reverseArray = [], length = numbers.length - 1, i = 0;
+    while(length >= 0) {
+        reverseArray[i] = numbers[length];
+        length--;
+        i++;
+    }
+
+    return reverseArray;
+}
+
+console.log(reverseArray(number));
+console.log(reverseArray(evenNumbers)); */
+
+//write a code to combine to array using concat
+/* console.log(number.concat(evenNumbers)); */
+
+
